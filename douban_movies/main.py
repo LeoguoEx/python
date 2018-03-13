45b9cc84840ba04fcd738f5f4e729f0a632dcd7b
Leoguo <903587975@qq.com>
2018-03-13 22:06:18 +0800

    容错和时间间隔
    请求网址添加容错和请求时间间隔机制
    （默认2秒）

--- douban_movies/main.py
+++ douban_movies/main.py
@@ -1,11 +1,23 @@
 from data import DouBanMovie
 from lxml import etree
+from request import UrlRequests
 
 def main(baseUrl = "", param = ""):
+    '''
     data = DouBanMovie();
     html = etree.parse('黑豹 (豆瓣).htm', etree.HTMLParser())
     data.parseHtml(html)
     data.print()
+    '''
+    request = UrlRequests()
+    request.addNeedRequestUrl('https://movie.douban.com/subject/3878007/?from=subject-page')
+    request.addNeedRequestUrl('111')
+    request.addNeedRequestUrl('https://movie.douban.com/subject/6390825/?from=showing')
+    request.addNeedRequestUrl('https://movie.douban.com/subject/3168101/?from=subject-page')
+    request.addNeedRequestUrl('000')
+
+    while(request.requestNext()):
+        request.request()
 
 if __name__ == "__main__":
     main()
--- douban_movies/request.py
+++ douban_movies/request.py
@@ -1,29 +1,29 @@
 #请求模块，用于请求对应的链接，使用requests库
 import requests
+import time
 
 class UrlRequests:
-
     need_request_url_set = set()
     requested_url_set = set()
+    SLEEP_TIME = 2.0
 
 
     def __init__(self):
         pass
 
-    def request(self):
+    def request(self, errorCount = 2):
         url = self.__getUrl__()
-        if url is not None:
-            try:
-                r = requests.get(url);
-                return r.text
-            except requests.RequestException as ex:
-                print(ex);
-        return None
+        return self.__request__(url)
 
     def addNeedRequestUrl(self, url):
-        allow = self.checkUrl(url)
+        allow = self.__checkUrl__(url)
         if allow:
             self.need_request_url_set.add(url)
+
+    def requestNext(self):
+        if len(self.need_request_url_set) > 0:
+            return True
+        return False
 
 
     def __checkUrl__(self, url):
@@ -37,3 +37,21 @@ class UrlRequests:
         if len(self.need_request_url_set) > 0:
             url = self.need_request_url_set.pop()
         return url;
+
+    def __request__(self, url, errorCount = 2):
+        if url is not None:
+            try:
+                print("go : {0}".format(url))
+                time.sleep(self.SLEEP_TIME)
+                r = requests.get(url);
+                return r.text
+            except requests.RequestException as ex:
+                print(ex);
+                if errorCount > 0:
+                    print("go1 : {0}    errorCount : {1}".format(url, errorCount))
+                    errorCount -= 1;
+                    return self.__request__(url, errorCount)
+                else:
+                    print("go2 : {0}".format(url))
+                    return self.request()
+        return None
